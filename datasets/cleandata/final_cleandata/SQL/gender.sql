2583e0f59d51455099e9a08efd5de38c82573d5e
natalie_davies <natalie.d.davies@outlook.com>
2020-11-10 06:22:42 +0800

    Added SQL file

---
+++ datasets/cleandata/final_cleandata/SQL/gender.sql
@@ -0,0 +1,11 @@
+create table gender (
+	Date varchar (500),
+	Sex varchar (500),
+	Location varchar (500),
+	"Industry division of main job" varchar (500),
+	"Employed full-time ('000)" decimal,
+	"Employed part-time ('000)" decimal
+);
+
+select * from gender;
+DROP TABLE gender;
